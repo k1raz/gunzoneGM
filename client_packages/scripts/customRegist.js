763082f38de5f650f1a4379363499c587fb9d7b3
k1raZ <funticwelson@gmail.com>
2022-11-14 22:05:20 +0300

    deleted other weapons, changed custom registration, changed respawn timer

--- client_packages/scripts/customRegist.js
+++ client_packages/scripts/customRegist.js
@@ -1,30 +1,6 @@
-// mp.events.add('incomingDamage', (sourceEntity, sourcePlayer, targetEntity, weapon, boneIndex, damage) => {
-//     mp.gui.chat.push('мама шлюха')
-//
-//     let hp = targetEntity.getHealth()
-//     if (weapon == 3249783761) {//Revolver
-//         if (boneIndex === 20) {
-//             if (hp <= 75) {
-//                 return;
-//             }
-//
-//             mp.players.local.applyDamageTo(75, true);
-//             setTimeout(() => {
-//             }, 1000);
-//             return
-//         } else {
-//             if (hp <= 50) {
-//                 return;
-//             }
-//             mp.players.local.applyDamageTo(50, true);
-//             setTimeout(() => {
-//             }, 1000);
-//         }
-//         return
-//     }
-// });
-
 mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapon, boneIndex, damage) => {
+
+    // revolver
     if (targetEntity.type === 'player' && targetEntity.weapon === 3249783761) {
         if (boneIndex === 20) {
             mp.game.weapon.setCurrentDamageEventAmount(75)
