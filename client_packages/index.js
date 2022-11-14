d267975f6aa16357a32c02afd5afccd7b57f9abc
k1raZ <funticwelson@gmail.com>
2022-11-14 18:34:57 +0300

    nameTags - custom name tag, custom registration - 50 body , 75 head (revolver)

--- client_packages/index.js
+++ client_packages/index.js
@@ -2,8 +2,6 @@ mp.game.audio.setAudioFlag("LoadMPData", true);
 mp.game.audio.requestScriptAudioBank("DLC_BIKER/BKR_KQ_01", false);
 global.adminGodMode = false;
 
-// require('./scripts/damageHandler/index')
-
 const toLoad = [
     "lib/scaleform_messages",
     "scripts/maxedStats",
@@ -12,7 +10,9 @@ const toLoad = [
     "scripts/wasted",
     "scripts/infiniteAmmo",
     "scripts/gameArea",
-    "scripts/fly"
+    "scripts/fly",
+    "scripts/nameTags",
+    "scripts/customRegist"
 ];
 
 
@@ -23,20 +23,3 @@ toLoad.forEach((file) => {
         mp.gui.chat.push(`Failed to load "${file}".`);
     }
 });
-
-// mp.nametags.orderByDistance = true
-// mp.nametags.useScreen2dCoords = true
-// mp.nametags.set({
-//     font: 0,
-//     outline: true,
-//     offset: 0.7,
-//     veh_offset: 1.0,
-//     color: [255, 255, 255, 255],
-//     size: 0.5,
-//
-//     hbar: {
-//         size: [15.06, 15.008],
-//         color: [255, 255, 255, 0],
-//         bg_color: [255, 255, 255, 0]
-//     },
-// });
